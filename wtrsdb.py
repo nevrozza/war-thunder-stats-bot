697cbc007abf0edcd29fe534c73464948187d31a
nevrozza <artemker667@gmail.com>
2022-07-14 04:29:50 +0300

    Update wtrsdb.py

--- wtrsdb.py
+++ wtrsdb.py
@@ -31,7 +31,6 @@ def parsing_of_squadrons():
     top_int = 0
     options = webdriver.ChromeOptions()
     
-    options.headless = True
     driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', options=options)
     driver.get("https://warthunder.com/en/community/clansleaderboard/")
     lnks=driver.find_elements('tag name', "a")

785b55e822b2ee71b17898c384923eb9eabf3325
nevrozza <artemker667@gmail.com>
2022-07-14 17:31:42 +0300

    Update wtrsdb.py

--- wtrsdb.py
+++ wtrsdb.py
@@ -415,11 +415,11 @@ def time_checker():
         func_parsing_of_squadrons_ts_start()
     elif str(str(int(time.strftime('%H', time.gmtime())) * 60 + (int(time.strftime('%M', time.gmtime()))))+ str('+'+time.strftime('%S', time.gmtime()))) in ['440+0', '1340+0']:
         func_parsing_of_squadrons_ts_last()    
-schedule.every(1).seconds.do(time_checker) 
+schedule.every(30).seconds.do(time_checker) 
 while True:
     
     if time.strftime('%H:%M:%S', time.gmtime()) == os.environ.get('time_start'):
         while True:
     
             schedule.run_pending()
-            time.sleep(30)
+            time.sleep(1)

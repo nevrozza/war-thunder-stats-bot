75d17a74f7e4ac54cac519e83f002eb10200accf
nevrozza <artemker667@gmail.com>
2022-07-14 20:21:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ async def on_message(message):
     if message.author == client.user:
          return
     if message.channel.name == 'управление':
-        if "<Role id=997106365413720104 name='123'>" in str(message.author.roles):
             if message.content.lower() == '!top':
                 await message.delete()
                 await message.channel.send(f'{message.author.mention} wait for top 20 players in <#996836535645241354>')
@@ -34,7 +33,7 @@ async def on_message(message):
                 fu.func_parsing_of_squadrons_ts_in_period()
                 
                 return
-        if message.content.lower() == '!check':
-            await message.delete()
-            await message.channel.send(f'{message.author.mention}: {message.author.roles}')
+            if message.content.lower() == '!check':
+                await message.delete()
+                await message.channel.send(f'{message.author.mention}: {message.author.roles}')
 client.run(BOT_TOKEN)

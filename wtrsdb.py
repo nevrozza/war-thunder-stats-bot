bad96f4c62054e8847630844849318b9bd093f6c
nevrozza <artemker667@gmail.com>
2022-07-15 21:45:57 +0300

    Update wtrsdb.py

--- wtrsdb.py
+++ wtrsdb.py
@@ -28,7 +28,7 @@ def time_checker():
 
     
     times = (str(str(int(time.strftime('%H', time.gmtime())) * 60 + (int(time.strftime('%M', time.gmtime()))))+ str('+'+time.strftime('%S', time.gmtime()))))
-    print(times)
+    
     if times in ['110+00', '140+00', '170+00', '200+00', '230+00', '260+00', '290+00', '320+00', '350+00', '380+00', '410+00', '860+00', '890+00', '920+00', '950+00', '980+00', '1010+00', '1040+00', '1070+00', '1100+00', '1130+00', '1160+00', '1190+00', '1220+00', '1250+00', '1280+00', '1310+00']:
         fu.func_parsing_of_squadrons_ts_in_period()
     elif times == '80+00':
